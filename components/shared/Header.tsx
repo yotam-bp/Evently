6773e88809a29ef84a88468b60ce9bd1896f903b
yotam-bp <yotyyybp@gmail.com>
2023-12-24 16:35:11 +0200

    nav items fix

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -2,7 +2,7 @@ import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
 import { Button } from "../ui/button"
-// import NavItems from "./NavItems"
+import NavItems from "./NavItems"
 import MobileNav from "./MobileNav"
 
 const Header = () => {
@@ -18,7 +18,7 @@ const Header = () => {
 
         <SignedIn>
           <nav className="md:flex-between hidden w-full max-w-xs">
-            {/* <NavItems /> */}
+            <NavItems />
           </nav>
         </SignedIn>
 
